3c8df5e8e5ab0889f1eb671efc0e07f562763128
Glenn Manguiat <karlo.manguiat@gmail.com>
2025-11-05 10:33:29 +0800

    Initial Commit

---
+++ Overtime/OvertimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OvertimeApp.swift
+//  Overtime
+//
+//  Created by Glenn Karlo Manguiat on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct OvertimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
