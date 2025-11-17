ca521c3b29c6a7614c63837d214c0235208d8e0d
Glenn Manguiat <karlo.manguiat@gmail.com>
2025-11-17 13:07:59 +0800

    - add mock sample entry - add list cell for cleaner code - add annual total - add fix for delete items to viewmodel

---
+++ Overtime/OvertimeListCell.swift
@@ -0,0 +1,18 @@
+//
+//  OvertimeListCell.swift
+//  Overtime
+//
+//  Created by Glenn Karlo Manguiat on 11/17/25.
+//
+
+import SwiftUI
+
+struct OvertimeListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OvertimeListCell()
+}
